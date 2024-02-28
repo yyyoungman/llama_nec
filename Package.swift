f20a411aabf35310ea06a141249cd1f5d659bc1c
chongyangming <chongyangming@kuaishou.com>
2024-02-28 14:10:33 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -45,7 +45,7 @@ let package = Package(
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
-                .define("GGML_USE_METAL"),
+//                .define("GGML_USE_METAL"),
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
                 // We should consider add this in the future when we drop support for iOS 14
                 // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
